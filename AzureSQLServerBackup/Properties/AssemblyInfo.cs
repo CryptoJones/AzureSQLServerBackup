679cba681b1d5441d93d313180c1a30a31a35d6e
Aaron K. Clark <akclark@atbashservices.com>
2017-11-15 14:07:14 -0600

    Desc Cleanup

--- AzureSQLServerBackup/Properties/AssemblyInfo.cs
+++ AzureSQLServerBackup/Properties/AssemblyInfo.cs
@@ -6,18 +6,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AzureSQLServerBackup")]
-[assembly: AssemblyDescription("https://github.com/CryptoJones/AzureSQLServerBackup")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AzureSQLServerBackup")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Azure/SQL Server 2014 Backup Utility")]
+[assembly: AssemblyCopyright("https://github.com/CryptoJones/AzureSQLServerBackup")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e7eee733-4091-4386-847c-1967984b54e3")]
